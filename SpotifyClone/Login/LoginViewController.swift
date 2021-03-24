ca4a9ea22ef871018097626a218dbc07826b1bbb
Guilherme Takakuwa <guilherme.takakuwa@vee.digital>
2021-03-24 10:05:05 -0300

    added  more content to info.plist and Added login in loginView

--- SpotifyClone/Login/LoginViewController.swift
+++ SpotifyClone/Login/LoginViewController.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+
+    @IBOutlet weak var registerNowButton: UIButton!
+    @IBOutlet weak var loginWithPhoneNumberButton: UIButton!
+    @IBOutlet weak var loginWithGoogleButton: UIButton!
+    @IBOutlet weak var loginWithFacebookButton: UIButton!
+    @IBOutlet weak var loginWithAppleButton: UIButton!
+    @IBOutlet weak var loginWithSpotifyButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func setButtonStyles() {
+        self.loginWithPhoneNumberButton.layer.borderWidth = 5;
+        self.loginWithPhoneNumberButton.layer.borderColor = UIColor.white.cgColor
+        self.loginWithPhoneNumberButton.layer.cornerRadius = 20
+
+        self.loginWithGoogleButton.layer.borderWidth = 5;
+        self.loginWithGoogleButton.layer.borderColor = UIColor.white.cgColor
+        
+        self.loginWithFacebookButton.layer.borderWidth = 5;
+        self.loginWithFacebookButton.layer.borderColor = UIColor.white.cgColor
+        
+        self.loginWithAppleButton.layer.borderWidth = 5;
+        self.loginWithAppleButton.layer.borderColor = UIColor.white.cgColor
+        
+        self.loginWithSpotifyButton.layer.borderWidth = 5;
+        self.loginWithSpotifyButton.layer.borderColor = UIColor.white.cgColor
+        
+    }
+}
+
---
+++ SpotifyClone/Login/LoginViewModel/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  SpotifyClone
+//
+//  Created by Guilherme Takakuwa on 23/03/21.
+//
+
+import Foundation
